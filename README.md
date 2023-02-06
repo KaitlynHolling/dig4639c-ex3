127420b8b59ddc3536ac97ef5af844d5b9c44a47
“KaitlynHolling” <kaitlynholling@gmail.com>
2023-02-06 10:50:06 -0500

    "Created quote app"

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dig4639c-ex3
+
+"The mind is everything, what you think you become" -Buddha
---
+++ index.js
@@ -0,0 +1,7 @@
+let quotes = [
+    "The way to get started is to quit talking and begin doing. -Walt Disney"
+]
+
+// get integer between 0 and 2
+
+console.log(quotes[Math.floor(Math.random()*3)])
